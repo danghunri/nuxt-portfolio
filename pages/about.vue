f78e4d6baee4210886f7d1d98a75b0e3e8ed8193
이상훈 <sanghoon0120@i-gns.co.kr>
2025-02-27 16:18:58 +0900

    init

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>This page is about me.</div>
+  <button @click="enableCustomLayout">Enable custom layout</button>
+</template>
+
+<script setup>
+// definePageMeta({
+//   layout: "another",
+// });
+
+function enableCustomLayout() {
+  setPageLayout("another");
+}
+</script>
---
+++ pages/blog/[id].vue
@@ -0,0 +1,8 @@
+<template>
+  <div>We are inside a blog!</div>
+  <div>Post id is {{ route.params.id }}</div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+</script>
