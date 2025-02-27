56c8f76c89f3ede067b298ae2d50571aca33b43b
이상훈 <sanghoon0120@i-gns.co.kr>
2025-02-27 18:24:33 +0900

    nuxt config disable future 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
-  future: {
-    compatibilityVersion: 4,
-  },
-  experimental: {
-    normalizeComponentNames: true,
-  },
+  // future: {
+  //   compatibilityVersion: 4,
+  // },
+  // experimental: {
+  //   normalizeComponentNames: true,
+  // },
 });
